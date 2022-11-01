8edc5060cdadd07f715abf856720b78d20dfd8a0
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2022-11-01 19:46:00 +0200

    Add project files.

---
+++ Generating 0 1 Vectors (generating combinations) Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Generating_0_1_Vectors__generating_combinations__Recursion
+{
+    internal class Program
+    {
+        static void Generate(int index, int[] arr)
+        {
+            if (index == arr.Length)
+            {
+                foreach (var item in arr)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    arr[index] = i;
+                    Generate(index + 1, arr);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+            Generate(0, arr);
+        }
+    }
+}
---
+++ TEST/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace TEST
+{
+    internal class Program
+    {
+        static int[] loops;
+        static int numberOfLoops;
+        static int stop;
+
+        static void Main(string[] args)
+        {
+            numberOfLoops = 2;
+            stop = 3;
+            loops = new int[numberOfLoops];
+            NestedLoops(0);
+
+
+        }
+        static void NestedLoops(int currentLoop)
+        {
+            if(currentLoop == numberOfLoops)
+            {
+                PrintLoops(loops);
+                return;
+            }
+
+            for (int i = 1; i <= stop; i++)
+            {
+                loops[currentLoop] = i;
+                NestedLoops(currentLoop+1);
+            }
+        }
+        static void PrintLoops(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
